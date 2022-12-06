235c4fade7fc9decf167117623cbfd1615abca35
astridjoh <Astrid.Johannsen@campus.lmu.de>
2022-12-06 23:30:05 +0100

    barchat.js working now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import logo from './logo.svg';
+import BarChart from './BarChart';
 import './App.css';
+
+const data = [
+  {month: "Jan 19", sales: 500, sales2: 300, sales3: 400},
+  {month: "Feb 19", sales: 700, sales2: 200, sales3: 200},
+  {month: "Mar 19", sales: 600, sales2: 300, sales3: 100},
+  {month: "Apr 19", sales: 400, sales2: 200, sales3: 300},
+  {month: "May 19", sales: 500, sales2: 200, sales3: 400},
+  {month: "Jun 19", sales: 800, sales2: 200, sales3: 100},
+  {month: "Jul 19", sales: 300, sales2: 300, sales3: 400},
+  {month: "Aug 19", sales: 400, sales2: 100, sales3: 300},
+  {month: "Sep 19", sales: 600, sales2: 200, sales3: 200},
+  {month: "Oct 19", sales: 400, sales2: 400, sales3: 200},
+  {month: "Nov 19", sales: 700, sales2: 100, sales3: 100},
+  {month: "Dec 19", sales: 400, sales2: 400, sales3: 300},
+  
+  {month: "Jan 20", sales: 500, sales2: 300, sales3: 400},
+  {month: "Feb 20", sales: 700, sales2: 200, sales3: 200},
+  {month: "Mar 20", sales: 600, sales2: 300, sales3: 100},
+  {month: "Apr 20", sales: 400, sales2: 200, sales3: 300},
+  {month: "May 20", sales: 500, sales2: 200, sales3: 400},
+  {month: "Jun 20", sales: 800, sales2: 200, sales3: 100},
+  {month: "Jul 20", sales: 300, sales2: 300, sales3: 400},
+  {month: "Aug 20", sales: 400, sales2: 100, sales3: 300},
+  {month: "Sep 20", sales: 600, sales2: 200, sales3: 200},
+  {month: "Oct 20", sales: 400, sales2: 400, sales3: 200},
+  {month: "Nov 20", sales: 700, sales2: 100, sales3: 100},
+  {month: "Dec 20", sales: 400, sales2: 400, sales3: 300},
+
+  {month: "Jan 21", sales: 500, sales2: 300, sales3: 400},
+  {month: "Feb 21", sales: 700, sales2: 210, sales3: 210},
+  {month: "Mar 21", sales: 600, sales2: 300, sales3: 100},
+  {month: "Apr 21", sales: 400, sales2: 210, sales3: 300},
+  {month: "May 21", sales: 500, sales2: 210, sales3: 400},
+  {month: "Jun 21", sales: 800, sales2: 210, sales3: 100},
+  {month: "Jul 21", sales: 300, sales2: 300, sales3: 400},
+  {month: "Aug 21", sales: 400, sales2: 100, sales3: 300},
+  {month: "Sep 21", sales: 600, sales2: 210, sales3: 210},
+  {month: "Oct 21", sales: 400, sales2: 400, sales3: 210},
+  {month: "Nov 21", sales: 700, sales2: 100, sales3: 100},
+  {month: "Dec 21", sales: 400, sales2: 400, sales3: 300},
+
+  {month: "Jan 22", sales: 500, sales2: 300, sales3: 400},
+  {month: "Feb 22", sales: 700, sales2: 220, sales3: 220},
+  {month: "Mar 22", sales: 600, sales2: 300, sales3: 100},
+  {month: "Apr 22", sales: 400, sales2: 220, sales3: 300},
+  {month: "May 22", sales: 500, sales2: 220, sales3: 400},
+  {month: "Jun 22", sales: 800, sales2: 220, sales3: 100},
+  {month: "Jul 22", sales: 300, sales2: 300, sales3: 400},
+  {month: "Aug 22", sales: 400, sales2: 100, sales3: 300},
+  {month: "Sep 22", sales: 600, sales2: 220, sales3: 220},
+  {month: "Oct 22", sales: 400, sales2: 400, sales3: 220},
+  {month: "Nov 22", sales: 700, sales2: 100, sales3: 100},
+  {month: "Dec 22", sales: 400, sales2: 400, sales3: 300},
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BarChart data={data} />
       </header>
     </div>
   );
---
+++ src/BarChart.js
@@ -0,0 +1,119 @@
+import React, { useRef, useEffect } from "react";
+import * as d3 from "d3";
+
+const BarChart = ({ data }) => {
+  const container = useRef(null);
+
+  // define dimensions of the chart
+  const width = 1000;
+  const height = 500;
+  const margin = { top: 20, right: 30, bottom: 30, left: 200 };
+
+  // define color scale
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+  useEffect(() => {
+    const svg = d3.select(container.current);
+
+    // create scales
+    const xScale = d3
+      .scaleBand()
+      .domain(data.map(d => d.month))
+      .range([0, width])
+      .padding(0.3);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.sales + d.sales2 + d.sales3)])
+      .range([height, 0]);
+
+    // create the stacked bars
+    const stackedData = d3.stack().keys(["sales", "sales2", "sales3"])(data);
+
+    // add bars
+    svg
+      .selectAll(".bar")
+      .data(stackedData)
+      .enter()
+      .append("g")
+      .attr("fill", d => color(d.key))
+      .selectAll("rect")
+      .data(d => d)
+      .enter()
+      .append("rect")
+      .attr("x", d => xScale(d.data.month))
+      .attr("y", d => yScale(d[1]))
+      .attr("height", d => yScale(d[0]) - yScale(d[1]))
+      .attr("width", xScale.bandwidth());
+
+    // add legend
+    const legend = svg
+      .append("g")
+      .attr("transform", `translate(${width - 100}, ${height - 100})`);
+
+    legend
+      .selectAll("rect")
+      .data(["sales", "sales2"])
+      .enter()
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", (d, i) => i * 20)
+      .attr("width", 10)
+      .attr("height", 10)
+      .attr("fill", d => color(d));
+
+    legend
+      .selectAll("text")
+      .data(["sales", "sales2"])
+      .enter()
+      .append("text")
+      .attr("x", 20)
+      .attr("y", (d, i) => i * 20 + 10)
+      .text(d => d);
+    // add axes
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale))
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("transform", "rotate(-65)");
+      
+    svg
+      .append("g")
+      .call(d3.axisLeft(yScale));
+  }, [data]);
+
+  return (
+    <div style={{ width: width, height: height, margin: "auto" }}>
+    <svg ref={container} style={{
+       overflow: "visible",
+       marginRight: "0px",
+       marginLeft: "0px", 
+       marginTop: "0px",
+       marginBottom: "0px", 
+       height: "100%",
+       width: "100%",
+    }}>
+      <style>
+        {`
+          rect.bar {
+            shape-rendering: crispEdges;
+          }
+
+          .axis path,
+          .axis line {
+            fill: none;
+            stroke: #000;
+            shape-rendering: crispEdges;
+          }
+        `}
+      </style>
+    </svg>
+    </div>
+  );
+};
+
+
+export default BarChart;
