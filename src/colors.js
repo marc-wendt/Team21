f7b2e914aae38130883e6be2a96c43ec2d65ac0e
johanna.schlechter <johanna.schlechter@outlook.com>
2022-12-07 15:25:14 +0100

    farbschema geupdated

--- src/colors.js
+++ src/colors.js
@@ -1,12 +1,12 @@
 const colors = {
-    hotels_inland: "#1f77b4",
-    hotels_ohne_garnis_inland: "#ff7f0e",
-    gasthöfe_inland: "#2ca02c",
-    pensionen_inland: "#d62728",
-    ferienhäuser_und_ferienwohnungen_inland: "#9467bd",
-    jugendherbergen_inland: "#8c564b",
-    campingplätze_inland: "#e377c2",
-    sonstige_inland: "#bcbd22",
+    hotels_inland: "#8dd3c7",
+    hotels_ohne_garnis_inland: "#ffffb3",
+    gasthöfe_inland: "#bebada",
+    pensionen_inland: "#fb8072",
+    ferienhäuser_und_ferienwohnungen_inland: "#80b1d3",
+    jugendherbergen_inland: "#fdb462",
+    campingplätze_inland: "#fccde5",
+    sonstige_inland: "#bc80bd",
   };
 
   export default colors;
