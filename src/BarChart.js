abf3e1e858b4b43d8e38d684e40f45052e79f91d
Marc <62611110+HuGo231@users.noreply.github.com>
2022-12-22 20:17:10 +0100

    commit

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,13 +1,14 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 
+
 const BarChart = ({ data, keys, colors }) => {
   const container = useRef(null);
 
   // define dimensions of the chart
   const width = 1000;
   const height = 500;
-  
+
   useEffect(() => {
     const svg = d3.select(container.current);
 
@@ -27,6 +28,89 @@ const BarChart = ({ data, keys, colors }) => {
     //const stackedData = d3.stack().keys(["hotels_inland", "hotels_ohne_garnis_inland", "gasthöfe_inland", "pensionen_inland", "ferienhäuser_und_ferienwohnungen_inland", "jugendherbergen_inland", "campingplätze_inland", "sonstige_inland"])(data);
     const stackedData = d3.stack().keys(keys)(data)
 
+        // Append two rect elements to the SVG element
+        svg.append('rect')
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('width', 300)
+          .attr('height', 500)
+          .style('fill', 'green');
+    
+        svg.append('rect')
+          .attr('x', 300)
+          .attr('y', 0)
+          .attr('width', 100)
+          .attr('height', 500)
+          .style('fill', 'red');
+    
+        svg.append('rect')
+          .attr('x', 400)
+          .attr('y', 0)
+          .attr('width', 100)
+          .attr('height', 500)
+          .style('fill', 'green');
+    
+        svg.append('rect')
+          .attr('x', 500)
+          .attr('y', 0)
+          .attr('width', 150)
+          .attr('height', 500)
+          .style('fill', 'red');
+    
+        svg.append('rect')
+          .attr('x', 650)
+          .attr('y', 0)
+          .attr('width', 120)
+          .attr('height', 500)
+          .style('fill', 'green');
+    
+        svg.append('rect')
+          .attr('x', 770)
+          .attr('y', 0)
+          .attr('width', 80)
+          .attr('height', 500)
+          .style('fill', 'red');
+    
+        svg.append('rect')
+          .attr('x', 850)
+          .attr('y', 0)
+          .attr('width', 150)
+          .attr('height', 500)
+          .style('fill', 'green'); 
+
+    // Function for this - but not working
+    // Select all the bars in the chart
+    // add bars
+    svg
+      .selectAll(".bar")
+      .data(stackedData)
+      .enter()
+      .append("g")
+      .attr("fill", d => colors[d.key])
+      .selectAll("rect")
+      .data(d => d)
+      .enter()
+      .append("rect")
+      .selectAll(".bar")
+      .each(function(d) {
+        // Append a rect element for the bar background
+        svg.select(this)
+          .append("rect")
+          .attr("class", "bar-bg")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("width", xScale(d.value))
+          .attr("height", 500)
+          .style("fill", "green");
+      });
+
+
+
+
+
+
+
+
     // add bars
     svg
       .selectAll(".bar")
@@ -42,6 +126,8 @@ const BarChart = ({ data, keys, colors }) => {
       .attr("y", d => yScale(d[1]))
       .attr("height", d => yScale(d[0]) - yScale(d[1]))
       .attr("width", xScale.bandwidth());
+
+
 
     // add legend
     const legend = svg
@@ -69,7 +155,7 @@ const BarChart = ({ data, keys, colors }) => {
       .style("fill", "white")
       .style("font-size", "12px")
       .text(d => d);
-      
+
     // add axes
     svg
       .append("g")
@@ -79,26 +165,28 @@ const BarChart = ({ data, keys, colors }) => {
       .style("text-anchor", "end")
       .attr("dx", "-.8em")
       .attr("transform", "rotate(-65)");
-      
+
     svg
       .append("g")
       .call(d3.axisLeft(yScale));
-       // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
+
+
 
   return (
     <div style={{ width: width, height: height, margin: "auto" }}>
-    <svg ref={container} style={{
-       overflow: "visible",
-       marginRight: "0px",
-       marginLeft: "0px", 
-       marginTop: "0px",
-       marginBottom: "0px", 
-       height: "100%",
-       width: "100%",
-    }}>
-      <style>
-        {`
+      <svg ref={container} style={{
+        overflow: "visible",
+        marginRight: "0px",
+        marginLeft: "0px",
+        marginTop: "0px",
+        marginBottom: "0px",
+        height: "100%",
+        width: "100%",
+      }}>
+        <style>
+          {`
           rect.bar {
             shape-rendering: crispEdges;
           }
@@ -110,8 +198,8 @@ const BarChart = ({ data, keys, colors }) => {
             shape-rendering: crispEdges;
           }
         `}
-      </style>
-    </svg>
+        </style>
+      </svg>
     </div>
   );
 };
