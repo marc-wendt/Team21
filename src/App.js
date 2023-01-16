156b5a2c8d4c39516440e279cc4ea32e1862b1b8
johanna.schlechter <johanna.schlechter@outlook.com>
2023-01-16 11:50:56 +0100

    small fix

--- src/App.js
+++ src/App.js
@@ -38,7 +38,16 @@ function App() {
         </StateContext.Provider>
       </div>
       <div className="description">
-        <p>Beschreibungstext zu Daten</p>
+        <h2>Beschreibungstext zu Daten:</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua.
+        </p>
       </div>
     </div>
   );
