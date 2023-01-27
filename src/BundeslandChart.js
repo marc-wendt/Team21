f78f565151491cb018dfcbc618952ab92f16613e
johanna.schlechter <johanna.schlechter@outlook.com>
2023-01-27 09:34:17 +0100

    small refactor

--- src/BundeslandChart.js
+++ src/BundeslandChart.js
@@ -8,23 +8,11 @@ import { colors, colorsBundesländer } from "./colors";
 import { keys, keysBundesländer } from "./keys";
 import { data as dataGermany } from "./data";
 import {
-  badenWürttemberg,
-  bayern,
-  berlin,
-  bremen,
-  hamburg,
-  hessen,
-  mecklenburgVorpommern,
-  niedersachsen,
-  nordrheinWestfalen,
-  rheinlandPfalz,
-  saarland,
-  sachsenAnhalt,
-  sachsen,
-  schleswigHolstein,
-  thüringen,
-  brandenburg,
-} from "./bundeslandData";
+  getBundeslandData,
+  getBundeslandInlandData,
+  getBundeslandAuslandData,
+  getEmptyBundeslandData,
+} from "./getData";
 
 function BundeslandChart() {
   const [isLoading, setLoading] = useState(true);
@@ -52,15 +40,15 @@ function BundeslandChart() {
 
   useEffect(() => {
     if (checkedAusland && checkedInland) {
-      getBundeslandData(currentState);
+      getBundeslandData(currentState, setData);
     } else if (checkedAusland && !checkedInland) {
-      getBundeslandAuslandData(currentState);
+      getBundeslandAuslandData(currentState, setData);
     } else if (checkedInland && !checkedAusland) {
-      getBundeslandInlandData(currentState);
+      getBundeslandInlandData(currentState, setData);
     } else if (!checkedInland && !checkedAusland) {
-      getEmptyBundeslandData();
+      getEmptyBundeslandData(setData);
     } else {
-      getBundeslandData(currentState);
+      getBundeslandData(currentState, setData);
     }
   }, [currentState, checkedAusland, checkedInland, checkedCoronaBg]);
 
@@ -79,210 +67,6 @@ function BundeslandChart() {
 
   // Data
   const [data, setData] = useState([]);
-
-  // get data (ausland and inland) for specific bundesland
-  const getBundeslandData = (bundesland) => {
-    switch (bundesland) {
-      case "Baden-Württemberg":
-        setData(badenWürttemberg);
-        break;
-      case "Bayern":
-        setData(bayern);
-        break;
-      case "Berlin":
-        setData(berlin);
-        break;
-      case "Brandenburg":
-        setData(brandenburg);
-        break;
-      case "Bremen":
-        setData(bremen);
-        break;
-      case "Hamburg":
-        setData(hamburg);
-        break;
-      case "Hessen":
-        setData(hessen);
-        break;
-      case "Mecklenburg-Vorpommern":
-        setData(mecklenburgVorpommern);
-        break;
-      case "Niedersachsen":
-        setData(niedersachsen);
-        break;
-      case "Nordrhein-Westfalen":
-        setData(nordrheinWestfalen);
-        break;
-      case "Rheinland-Pfalz":
-        setData(rheinlandPfalz);
-        break;
-      case "Saarland":
-        setData(saarland);
-        break;
-      case "Sachsen-Anhalt":
-        setData(sachsenAnhalt);
-        break;
-      case "Sachsen":
-        setData(sachsen);
-        break;
-      case "Schleswig-Holstein":
-        setData(schleswigHolstein);
-        break;
-      case "Thüringen":
-        setData(thüringen);
-        break;
-      default:
-        break;
-    }
-  };
-
-  // get only inland data for specific bundesland
-  const getBundeslandInlandData = (bundesland) => {
-    let d;
-
-    switch (bundesland) {
-      case "Baden-Württemberg":
-        d = badenWürttemberg;
-        break;
-      case "Bayern":
-        d = bayern;
-        break;
-      case "Berlin":
-        d = berlin;
-        break;
-      case "Brandenburg":
-        d = brandenburg;
-        break;
-      case "Bremen":
-        d = bremen;
-        break;
-      case "Hamburg":
-        d = hamburg;
-        break;
-      case "Hessen":
-        d = hessen;
-        break;
-      case "Mecklenburg-Vorpommern":
-        d = mecklenburgVorpommern;
-        break;
-      case "Niedersachsen":
-        d = niedersachsen;
-        break;
-      case "Nordrhein-Westfalen":
-        d = nordrheinWestfalen;
-        break;
-      case "Rheinland-Pfalz":
-        d = rheinlandPfalz;
-        break;
-      case "Saarland":
-        d = saarland;
-        break;
-      case "Sachsen-Anhalt":
-        d = sachsenAnhalt;
-        break;
-      case "Sachsen":
-        d = sachsen;
-        break;
-      case "Schleswig-Holstein":
-        d = schleswigHolstein;
-        break;
-      case "Thüringen":
-        d = thüringen;
-        break;
-      default:
-        d = [];
-        break;
-    }
-
-    let arr = [];
-    d.forEach(function (el) {
-      arr.push({
-        month: el.month,
-        inland: el.inland,
-        ausland: 0,
-        hotels: el.hotels,
-      });
-    });
-
-    setData(arr);
-  };
-
-  // get only ausland data for specific bundesland
-  const getBundeslandAuslandData = (bundesland) => {
-    let d;
-
-    switch (bundesland) {
-      case "Baden-Württemberg":
-        d = badenWürttemberg;
-        break;
-      case "Bayern":
-        d = bayern;
-        break;
-      case "Berlin":
-        d = berlin;
-        break;
-      case "Brandenburg":
-        d = brandenburg;
-        break;
-      case "Bremen":
-        d = bremen;
-        break;
-      case "Hamburg":
-        d = hamburg;
-        break;
-      case "Hessen":
-        d = hessen;
-        break;
-      case "Mecklenburg-Vorpommern":
-        d = mecklenburgVorpommern;
-        break;
-      case "Niedersachsen":
-        d = niedersachsen;
-        break;
-      case "Nordrhein-Westfalen":
-        d = nordrheinWestfalen;
-        break;
-      case "Rheinland-Pfalz":
-        d = rheinlandPfalz;
-        break;
-      case "Saarland":
-        d = saarland;
-        break;
-      case "Sachsen-Anhalt":
-        d = sachsenAnhalt;
-        break;
-      case "Sachsen":
-        d = sachsen;
-        break;
-      case "Schleswig-Holstein":
-        d = schleswigHolstein;
-        break;
-      case "Thüringen":
-        d = thüringen;
-        break;
-      default:
-        d = [];
-        break;
-    }
-
-    let arr = [];
-    d.forEach(function (el) {
-      arr.push({
-        month: el.month,
-        inland: 0,
-        ausland: el.ausland,
-        hotels: el.hotels,
-      });
-    });
-
-    setData(arr);
-  };
-
-  // gets empty array
-  const getEmptyBundeslandData = () => {
-    let arr = [];
-    setData(arr);
-  };
 
   if (isLoading)
     return (
