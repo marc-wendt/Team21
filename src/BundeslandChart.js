db461102ae7e783f48d8211c42673867a93cf2f7
johanna.schlechter <johanna.schlechter@outlook.com>
2023-01-27 12:16:58 +0100

    filters are now working

--- src/BundeslandChart.js
+++ src/BundeslandChart.js
@@ -6,7 +6,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { StateContext, MapContext } from "./App";
 import { colors, colorsBundesländer } from "./colors";
 import { keys, keysBundesländer } from "./keys";
-import { data as dataGermany } from "./data";
+import { dataGermany } from "./data";
 import {
   getBundeslandData,
   getBundeslandInlandData,
@@ -33,10 +33,25 @@ function BundeslandChart() {
     }
   }, [checkedMap]);
 
+  // Data
+  const [data, setData] = useState([]);
+  const [dataG, setDataGermany] = useState([]);
+
   // Checkbox
   const [checkedInland, setCheckedInland] = useState(true);
   const [checkedAusland, setCheckedAusland] = useState(true);
   const [checkedCoronaBg, setCheckedCoronaBg] = useState(false);
+
+  const [checkboxesBeherbergung, setCheckboxesBeherbergung] = useState({
+    hotelsOhneGarnis: true,
+    hotels: true,
+    gasthöfe: true,
+    pensionen: true,
+    ferienhäuser: true,
+    jugendherbergen: true,
+    campingplätze: true,
+    sonstige: true,
+  });
 
   useEffect(() => {
     if (checkedAusland && checkedInland) {
@@ -65,8 +80,55 @@ function BundeslandChart() {
     setCheckedCoronaBg(!checkedCoronaBg);
   };
 
-  // Data
-  const [data, setData] = useState([]);
+  useEffect(() => {
+    let d = dataGermany;
+    let arr = [];
+
+    d.forEach(function (el) {
+      arr.push({
+        month: el.month,
+        hotels_ohne_garnis_inland: checkboxesBeherbergung.hotelsOhneGarnis
+          ? el.hotels_ohne_garnis_inland
+          : 0,
+        hotels_ohne_garnis_ausland: el.hotels_ohne_garnis_ausland,
+        hotels_inland: checkboxesBeherbergung.hotels ? el.hotels_inland : 0,
+        hotels_ausland: el.hotels_ausland,
+        gasthöfe_inland: checkboxesBeherbergung.gasthöfe
+          ? el.gasthöfe_inland
+          : 0,
+        gasthöfe_ausland: el.gasthöfe_ausland,
+        pensionen_inland: checkboxesBeherbergung.pensionen
+          ? el.pensionen_inland
+          : 0,
+        pensionen_ausland: el.pensionen_ausland,
+        ferienhäuser_und_ferienwohnungen_inland:
+          checkboxesBeherbergung.ferienhäuser
+            ? el.ferienhäuser_und_ferienwohnungen_inland
+            : 0,
+        ferienhäuser_und_ferienwohnungen_ausland:
+          el.ferienhäuser_und_ferienwohnungen_ausland,
+        jugendherbergen_inland: checkboxesBeherbergung.jugendherbergen
+          ? el.jugendherbergen_inland
+          : 0,
+        jugendherbergen_ausland: el.jugendherbergen_ausland,
+        campingplätze_inland: checkboxesBeherbergung.campingplätze
+          ? el.campingplätze_inland
+          : 0,
+        campingplätze_ausland: el.campingplätze_ausland,
+        sonstige_inland: checkboxesBeherbergung.sonstige
+          ? el.sonstige_inland
+          : 0,
+        sonstige_ausland: el.sonstige_ausland,
+        insgesamt_inland: el.insgesamt_inland,
+        insgesamt_ausland: el.insgesamt_ausland,
+      });
+    });
+    setDataGermany(arr);
+  }, [checkboxesBeherbergung, setDataGermany]);
+
+  const handleCheckboxBeherbergungChange = (name, checked) => {
+    setCheckboxesBeherbergung({ ...checkboxesBeherbergung, [name]: !checked });
+  };
 
   if (isLoading)
     return (
@@ -76,11 +138,92 @@ function BundeslandChart() {
             Ankünfte von Inländern pro Beherbergungsart für gesamt Deutschland
           </p>
         </div>
-        <BarChart data={dataGermany} keys={keys} colors={colors} />
+        <BarChart data={dataG} keys={keys} colors={colors} />
         <br></br>
         <div className="textHeader">
           <p className="legendHeader"> Legende:</p>
+          <p className="filterHeader"> Filter: </p>
         </div>
+        <CheckBox
+          label="Hotels"
+          value={checkboxesBeherbergung.hotels}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "hotels",
+              checkboxesBeherbergung.hotels
+            )
+          }
+        />
+        <CheckBox
+          label="Hotels ohne Garnis"
+          value={checkboxesBeherbergung.hotelsOhneGarnis}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "hotelsOhneGarnis",
+              checkboxesBeherbergung.hotelsOhneGarnis
+            )
+          }
+        />
+        <CheckBox
+          label="Gasthöfe"
+          value={checkboxesBeherbergung.gasthöfe}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "gasthöfe",
+              checkboxesBeherbergung.gasthöfe
+            )
+          }
+        />
+        <CheckBox
+          label="Pensionen"
+          value={checkboxesBeherbergung.pensionen}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "pensionen",
+              checkboxesBeherbergung.pensionen
+            )
+          }
+        />
+        <CheckBox
+          label="Ferienhäuser und Ferienwohnungen"
+          value={checkboxesBeherbergung.ferienhäuser}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "ferienhäuser",
+              checkboxesBeherbergung.ferienhäuser
+            )
+          }
+        />
+        <CheckBox
+          label="Jugendherbergen"
+          value={checkboxesBeherbergung.jugendherbergen}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "jugendherbergen",
+              checkboxesBeherbergung.jugendherbergen
+            )
+          }
+        />
+        <CheckBox
+          label="Campingplätze"
+          value={checkboxesBeherbergung.campingplätze}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "campingplätze",
+              checkboxesBeherbergung.campingplätze
+            )
+          }
+        />
+        <CheckBox
+          label="Sonstige"
+          value={checkboxesBeherbergung.sonstige}
+          onChange={() =>
+            handleCheckboxBeherbergungChange(
+              "sonstige",
+              checkboxesBeherbergung.sonstige
+            )
+          }
+        />
       </div>
     );
 
